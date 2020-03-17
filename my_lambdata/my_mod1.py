4d5db3a10b7168f35b08cc637fbdcc7e265829ac
Todd DiFronzo <fonze@aol.com>
2020-03-17 15:04:31 -0700

    OOP last

--- my_lambdata/my_mod1.py
+++ my_lambdata/my_mod1.py
@@ -8,3 +8,10 @@ from my_lambdata.my_mod1 import enlarge
 
 x = 5
 print("Enlarge", x, "TO", enlarge(x))
+
+# or
+# from my_lambdata import my_mod1
+# 
+# x = 5
+# print("ENLARGE", x, "TO", my_mod1.enlarge(x))
+

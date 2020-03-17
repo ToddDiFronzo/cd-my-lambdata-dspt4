909643ea7bbe74acb7e3c69648c546ba2af5f2b8
Todd DiFronzo <fonze@aol.com>
2020-03-17 12:42:06 -0700

    Add docstring

--- my_lambdata/my_mod.py
+++ my_lambdata/my_mod.py
@@ -1,16 +1,23 @@
 def enlarge(n):
+	"""
+	Multiple a number by 100
+
+	Param: n (numeric/float/int) the number to enlarge
+
+	Return the enlarge number (numeric)
+	"""
 	return n*100
 	
 print("HI")
 
 if __name__ == "__main__":
-	# only if run from the command line, do the following code:
-	# otherwise dont
-	print("JUNK")
-	print("Global JUNK")
+# 	# only if run from the command line, do the following code:
+# 	# otherwise dont
+# 	print("JUNK")
+# 	print("Global JUNK")
 	
-	y = float(input("Please input a number to enlarge: "))
-	print(enlarge(y))
+# 	y = float(input("Please input a number to enlarge: "))
+# 	print(enlarge(y))
 
 def funcname(self, parameter_list):
 	pass

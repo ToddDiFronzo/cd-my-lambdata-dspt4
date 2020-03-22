7e4857d63a34715692b864a83b5ecc7d9a60a995
Todd DiFronzo <fonze@aol.com>
2020-03-22 00:44:28 -0700

    with all states added

--- my_lambdata/assignment4.py
+++ my_lambdata/assignment4.py
@@ -11,28 +11,63 @@ class CustomFrame(pandas.DataFrame):
         # type(new_df["abbrev"]) #> <class 'pandas.core.series.Series'>
         # see: https://pandas.pydata.org/pandas-docs/stable/reference/api/pandas.Series.map.html
         names_map = {
-            "CA": "Cali",
-            "CT": "Conn",
-            "CO": "Colorado"
-            # todo: more abbrevs!
-        }
+            "AL": "Alabama",
+            "AK": "Alaska",
+            "AZ": "Arizona",
+            "AR": "Arkanasas",
+            "CA": "California",
+            "CT": "Connecticut",
+            "CO": "Colorado",
+            "DC": "Washington D.C.",
+            "DE": "Delaware",
+            "FL": "Florida",
+            "GA": "Georgia",
+            "HI": "Hawaii",
+            "ID": "Idaho",
+            "IL": "Illinois",
+            "IN": "Indiana",
+            "IA": "Iowa",
+            "KS": "Kansas",
+            "KY": "Kentucky",
+            "LA": "Louisiana",
+            "ME": "Maine",
+            "MD": "Maryland",
+            "MA": "Massachusetts",
+            "MI": "Michigan",
+            "MN": "Minnesota",
+            "MS": "Mississippi",
+            "MO": "Missouri",
+            "MT": "Montana",
+            "NE": "Nebraska",
+            "NV": "Nevada",
+            "NH": "New Hampshire",
+            "NJ": "New Jersey",
+            "NM": "New Mexico",
+            "NY": "New York",
+            "NC": "North Carolina",
+            "ND": "North Dakota",
+            "OH": "Ohio",
+            "OK": "Oklahoma",
+            "OR": "Oregon",
+            "PA": "Pennsylvania",
+            "RI": "Rhode Island",
+            "SC": "South Carolina",
+            "SD": "South Dakota",
+            "TN": "Tennessee",
+            "TX": "Texas",
+            "UT": "Utah",
+            "VT": "Vermont",
+            "VA": "Virginia",
+            "WA": "Washington",
+            "WV": "West Virginia",
+            "WI": "Wisconsin",
+            "WY": "Wyoming"}
         self["name"] = self["abbrev"].map(names_map)
 if __name__ == "__main__":
     print("--------------")
-    #df1 = pandas.DataFrame({"abbrev": ["CA", "CT", "CO", "TX", "DC"]})
-    #print(df1.head())
-    #new_df = add_state_names(df1)
-    #print(new_df.head())
-    #processor = DataProcessor(df1)
-    #print(processor.df.head())
-    #processor.add_state_names()
-    #print(processor.df.head())
+
     custom_df = CustomFrame({"abbrev": ["CA", "CT", "CO", "TX", "DC"]})
     print(custom_df.head())
     custom_df.add_state_names()
     print(custom_df.head())
-    #print("--------------")
-    #df2 = pandas.DataFrame({"abbrev": ["OH", "MI", "CO", "TX", "PA"]})
-    #print(df2.head())
-    #new_df2 = add_state_names(df2)
-    #print(new_df2.head())
+    

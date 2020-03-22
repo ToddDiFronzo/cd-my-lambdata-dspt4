5ca46f3d320471686809f38108ad5019bc8260ad
Todd DiFronzo <fonze@aol.com>
2020-03-21 17:06:00 -0700

    Try script now

--- my_lambdata/my_script.py
+++ my_lambdata/my_script.py
@@ -1,6 +1,6 @@
 import pandas
 
-from cd-my-lambdata-dspt4.my_mod import enlarge
+from my_lambdata.my_mod import enlarge
 
 print("HI")
 
@@ -8,4 +8,4 @@ df = pandas.DataFrame({"x":[1,2,3], "y":[4,5,6]})
 print(df.head())
 
 x = 5
-print("Enlarge", x, "TO"< enlarge(x))
+print("Enlarge", x, "TO", enlarge(x))

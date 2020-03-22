a8e96b4ed71f0a373e9ad63c976586f0a53d8a45
Todd DiFronzo <fonze@aol.com>
2020-03-22 00:19:48 -0700

    Upload states abbrev function

--- my_lambdata/assignment.py
+++ my_lambdata/assignment.py
@@ -79,13 +79,13 @@ def add_state_names(my_df):
 if __name__ == "__main__":
 
     print("---------------")
-    df = pandas.DataFrame({"abbrev": ["CA", "CT", "CO", "TX", "DC"]})
+    df = pandas.DataFrame({"abbrev": ["CA", "CT", "CO", "TX", "DC", "NV", "FL"]})
     print(df.head())
     new_df = add_state_names(df)
     print(new_df.head())
 
     print("---------------")
-    df2 = pandas.DataFrame({"abbrev": ["OH", "MI", "CO", "TX", "PA"]})
+    df2 = pandas.DataFrame({"abbrev": ["OH", "MI", "CO", "TX", "PA", "AK", "WY"]})
     print(df2.head())
     new_df2 = add_state_names(df2)
     print(new_df2.head())

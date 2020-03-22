38459e1db6e204c6f2a9dd71edc3ef2da64a1d72
Todd DiFronzo <fonze@aol.com>
2020-03-21 18:18:08 -0700

    added conditional

--- my_lambdata/my_mod.py
+++ my_lambdata/my_mod.py
@@ -5,9 +5,20 @@ def enlarge(n):
 	return n * 100
 
 
-print("Junk")
-print("Globabl Scope")
+# don't do the below with other code, it messes up the script when you run so do what is after it
+# print("Junk")
+# print("Globabl Scope")
 
-y = float(input("Please input a number to enlarge: "))
-print(enlarge(y))
+# y = float(input("Please input a number to enlarge: "))
+# print(enlarge(y))
 
+
+# do this instead
+if __name__ == "__main__":
+	# only if run from the command line, invoke the follwoing code
+	# otherwise don't
+	print("Junk")
+	print("Globabl Scope")
+
+	y = float(input("Please input a number to enlarge: "))
+	print(enlarge(y))
